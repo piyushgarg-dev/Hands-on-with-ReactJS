f0ee2cf3da9643293c13cd2158eca49a25687b83
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-07-08 23:16:52 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+      todos:[
+        {
+          id:1,
+          title:'Meet Boss',
+          completed:true
+        },
+        {
+          id:2,
+          title:'Sleep',
+          completed:true
+        },
+        {
+          id:3,
+          title:'Repeat',
+          completed:true
+        },
+      ]
+  }
+  render(){
+   
+    return (
+      <div className="App">
+      <h1>Todos</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Todos extends React.Component{
+    render(){
+        return this.props.todos.map((todo)=>(
+            <h3>{todo.title}</h3>
+        ));
+    }
+   
+}
+
+export default Todos;
